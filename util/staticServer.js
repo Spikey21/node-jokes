1150bec3e69d2e316f2910fdb67c65a68dd6d9bc
Spikey21 <rafal.paciorek19@gmail.com>
2024-11-12 21:13:52 +0100

    add parse pathname

--- util/staticServer.js
+++ util/staticServer.js
@@ -14,8 +14,28 @@ const mimeTypes = {
 // reg: /app.js
 function serveStaticFile(req, res) {
     const baseURL = req.protocol + "://" + req.headers.host + "/"
-    const parseURL = new URL(req.url, baseURL)
-    console.log(parseURL);
+    const parsedURL = new URL(req.url, baseURL)
+    let pathSanitize = path.normalize(parsedURL.pathname)
+    let pathname = path.join(__dirname, "..", "static", pathSanitize)
+    
+    if (fs.existsSync(pathname)) {
+        if (fs.statSync(pathname).isDirectory()) {
+            pathname+="/index.html"
+        }
+        fs.readFile(pathname, function (err, data) {
+            if (err) {
+                res.statusCode = 500
+                res.end("Error! File not found: ", err)
+            } else {
+                const extension = path.parse(pathname).ext
+                res.setHeader("Content-type", mimeTypes[extension])
+                res.end(data)
+            }
+        })
+    } else {
+        res.statusCode = 404
+        res.end("Error! File not found")
+    }
 }
 
 module.exports = {
